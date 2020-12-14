15b1c225d52d3152b60335c3e1abdfecad7ad512
Eric <ericmiranda7@gmail.com>
2020-12-14 12:09:01 +0530

    phonebook step4

--- index.js
+++ index.js
@@ -39,5 +39,11 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${notes.length} people</p> ${date}`)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = +request.params.id
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
