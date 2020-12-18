3922e8c4846d347add0f7deb50e351a0b5c810d6
Eric <ericmiranda7@gmail.com>
2020-12-18 09:43:41 +0530

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://vast-garden-49163.herokuapp.com/
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 const morgan = require('morgan')
