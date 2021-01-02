012c9cadf820138f29aa8baf35dc3130aee1a9fa
Eric <ericmiranda7@gmail.com>
2021-01-02 18:56:43 +0530

    uibuild

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const Phonebook = require('./models/phonebook')
-app.use(express.json())
-app.use(express.static('build'))
-app.use(cors())
-
 const morgan = require('morgan')
-const { Mongoose } = require('mongoose')
 morgan.token('data', (req, res) => {
     return JSON.stringify(req.body)
 })
 const morganApp = morgan(':method :url :status :res[content-length] - :response-time ms :data')
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 app.use(morganApp)
 
 app.get('/', (request, response) => {
@@ -23,9 +22,14 @@ app.get('/api/persons', (request, response) => {
     Phonebook.find({}).then(phonebook => response.json(phonebook))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    Phonebook.findById(id).then(person => response.json(person))
+    Phonebook.findById(id)
+    .then(person => {
+        if (person) response.json(person)
+        else response.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -33,9 +37,13 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${phonebook.length} people</p> ${date}`)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = +request.params.id
-    Phonebook.findById(id).then(person => response.json(person))
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    Phonebook.findByIdAndRemove(id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -56,6 +64,34 @@ app.post('/api/persons', (request, response) => {
         console.log('saved')
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+    }
+
+    Phonebook.findByIdAndUpdate(id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unkownEndPoint = (request, response) => {
+    response.status(404).send({error: 'unkown endpoint'})
+}
+app.use(unkownEndPoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name == 'CastError') response.status(400).send({error: 'malformatted id'})
+    else next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
